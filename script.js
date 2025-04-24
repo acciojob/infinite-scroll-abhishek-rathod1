35419fca8d8976d2f79d9fe47577cc29cff499ae
Abhishek Rathod <118195093+abhishek-rathod1@users.noreply.github.com>
2025-04-24 13:22:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 //your code here!
+const olist = document.getElementById("infi-list");
+let itemCount = 0;
 
+function addListItem(count) {
+	for (let i = 0; i < count; i++) {
+		itemCount++;
+		const li = document.createElement('li');
+		li.textContent = `Item ${itemCount}`
+		olist.appendChild(li);		
+	}	
+}
+//add initial 10 item
+addListItem(10);
+// addListItem(10);
+
+
+
+function handleScroll() {
+	const{scrollTop, scrollHeight, clientHeight} = document.documentElement;
+	if(scrollTop + clientHeight >= scrollHeight - 5 ){
+		console.log("scroll");
+		addListItem(2);
+	}
+	
+}
+window.addEventListener('scroll',handleScroll);
